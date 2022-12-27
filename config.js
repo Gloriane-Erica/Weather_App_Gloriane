1789cd75b7566f591c38d41626ef1eb1461ae1f2
Gloriane-Erica <icahgloriane@gmail.com>
2022-12-27 17:43:47 +0800

    First commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "d507bb7dff1790805781d167367402dc"
+    }
+}
+
+module.exports = constants;
